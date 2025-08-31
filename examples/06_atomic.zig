8cb0607211765002344e3b3ad60353c6cdabcecf
Pontus <pontus.rosqvist@gmail.com>
2025-08-31 14:48:50 +0200

    add atomic example

---
+++ examples/06_atomic.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const Irc = @import("irc").Irc;
+
+pub fn main() !void {
+    // Boilerplate, set up allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const slice = try Irc(.Slice, u8, .{ .atomic = true }).init(allocator, 5);
+    //                                    |_ The reference count is now only ever atomically modified
+    defer slice.deinit(allocator);
+
+    try slice.retain();
+
+    // Do stuff with slice
+    @memcpy(slice.items, "12345");
+    std.debug.print(
+        \\The reference count can be set to be atomic, i.e. the
+        \\Irc can be shared in multiple threads. By default it
+        \\is not atomic:
+        \\
+        \\      default:  atomic={}
+        \\      this one: atomic={}
+        \\
+        \\if a type has an atomic counter it cannot be cast away
+        \\and vice versa.
+    ,
+        .{ Irc(.Slice, u8, .{}).config.atomic, @TypeOf(slice).config.atomic },
+    );
+
+    slice.release();
+}
